9f95c22c55453f0ce7140d6709b1affa4ecadbad
atmos <atmos@atmos.org>
2008-06-03 21:41:08 -0600

    code to handle things

---
+++ URLHandlerCommand.h
@@ -0,0 +1,10 @@
+// file URLHandlerCommand.h
+
+#import <Foundation/Foundation.h>
+#import <Foundation/NSTask.h>
+
+@interface URLHandlerCommand : NSScriptCommand {
+
+}
+
+@end
---
+++ URLHandlerCommand.m
@@ -0,0 +1,29 @@
+// file URLHandlerCommand.m
+#import "URLHandlerCommand.h"
+
+@implementation URLHandlerCommand
+
+- (id)performDefaultImplementation {
+  NSTask *task;
+
+  NSString *urlString = [self directParameter];
+	NSArray *values = [urlString componentsSeparatedByString :@"/"];
+  NSLog(@"url = %@", urlString);
+
+  NSLog(@"url = %@", values);
+	NSLog(@"cluster = %@", [values objectAtIndex:2]);
+	NSLog(@"environment = %@", [values objectAtIndex:3]);
+  
+  NSArray *arguments;
+  arguments = [NSArray arrayWithObjects: @"terminal", @"-C", [values objectAtIndex:2], @"-E", [values objectAtIndex:3], nil];
+  NSLog(@"args = %@", arguments);
+
+  task = [[NSTask alloc] init];
+  [task setLaunchPath: @"/opt/local/bin/samurai"];
+  [task setArguments: arguments];
+  [task launch];
+
+  return nil;
+}
+
+@end
