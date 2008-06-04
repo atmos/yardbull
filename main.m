2148ab85dbfb6480c662c6b6f4e660a25cc522d3
Corey Donohoe <atmos@corey-donohoes-macbook-air.local>
2008-06-04 09:50:39 -0700

    little more logging, tracking down weirdnesses

--- main.m
+++ main.m
@@ -3,12 +3,13 @@
 //  YardBulls
 //
 //  Created by Corey Donohoe on 6/3/08.
-//  Copyright __MyCompanyName__ 2008. All rights reserved.
+//  Copyright EngineYard 2008. All rights reserved.
 //
 
 #import <Cocoa/Cocoa.h>
 
 int main(int argc, char *argv[])
 {
-    return NSApplicationMain(argc,  (const char **) argv);
+  NSLog(@"Started up...")
+  return NSApplicationMain(argc,  (const char **) argv);
 }
